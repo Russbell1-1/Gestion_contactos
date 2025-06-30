491a2eb3a84912e11423a7af083c1f804ae5efef
Russbell1-1 <zvalkiria@outlook.es>
2025-06-30 12:09:03 -0500

    Agrega funcionalidad: Mostrar contactos

--- Ejercicio 1.cpp
+++ Ejercicio 1.cpp
@@ -7,22 +7,6 @@ struct contactoEmail{
 	int edad;
 };
 
-void eliminarContacto(contactoEmail contactos[],int &n){
-	if(n==0)return;
-	string correo;
-	cout<<"Ingrese el email del contacto a eliminar: ";
-	cin>>correo;
-	for(int i=0;i<n;i++){
-		if(contactos[i].email==correo){
-			for(int j=i;j<n-1;j++){
-				contactos[j]=contactos[j+1];
-			}
-			n--;
-			break;
-		}
-	}
-}
-
 void agregarContacto(contactoEmail contactos[],int &n){
 	if(n<100){
 		cout<<"Nombre completo: ";
@@ -41,15 +25,43 @@ void agregarContacto(contactoEmail contactos[],int &n){
 	}
 }
 
+void eliminarContacto(contactoEmail contactos[],int &n){
+	if(n==0)return;
+	string correo;
+	cout<<"Ingrese el email del contacto a eliminar: ";
+	cin>>correo;
+	for(int i=0;i<n;i++){
+		if(contactos[i].email==correo){
+			for(int j=i;j<n-1;j++){
+				contactos[j]=contactos[j+1];
+			}
+			n--;
+			break;
+		}
+	}
+}
+
+void mostrarContactos(contactoEmail contactos[],int n){
+	for(int i=0;i<n;i++){
+		cout<<"Nombre: "<<contactos[i].nombre<<endl;
+		cout<<"Sexo: "<<contactos[i].sexo<<endl;
+		cout<<"Edad: "<<contactos[i].edad<<endl;
+		cout<<"Telefono: "<<contactos[i].telefono<<endl;
+		cout<<"Email: "<<contactos[i].email<<endl;
+		cout<<"Nacionalidad: "<<contactos[i].nacionalidad<<endl<<endl;
+	}
+}
+
 int main(){
 	contactoEmail contactos[100];
 	int n=0,op;
 	do{
-		cout<<"1. Agregar contacto\n2. Eliminar un contacto\nOpcion: ";
+		cout<<"1. Agregar contacto\n2. Eliminar contacto\n3. Mostrar contactos\nOpcion: ";
 		cin>>op;
 		switch(op){
 			case 1:agregarContacto(contactos,n);break;
 			case 2:eliminarContacto(contactos,n);break;
+			case 3:mostrarContactos(contactos,n);break;
 		}
 	}while(true);
 	return 0;
