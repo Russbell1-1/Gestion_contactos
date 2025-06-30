d0a5157202fc6ac8e551cb14dfedb1bdc9348752
Russbell1-1 <zvalkiria@outlook.es>
2025-06-30 12:04:28 -0500

    Agrega funcionalidad: Eliminar contacto

--- Ejercicio 1.cpp
+++ Ejercicio 1.cpp
@@ -7,6 +7,22 @@ struct contactoEmail{
 	int edad;
 };
 
+void eliminarContacto(contactoEmail contactos[],int &n){
+	if(n==0)return;
+	string correo;
+	cout<<"Ingrese el email del contacto a eliminar: ";
+	cin>>correo;
+	for(int i=0;i<n;i++){
+		if(contactos[i].email==correo){
+			for(int j=i;j<n-1;j++){
+				contactos[j]=contactos[j+1];
+			}
+			n--;
+			break;
+		}
+	}
+}
+
 void agregarContacto(contactoEmail contactos[],int &n){
 	if(n<100){
 		cout<<"Nombre completo: ";
@@ -29,10 +45,11 @@ int main(){
 	contactoEmail contactos[100];
 	int n=0,op;
 	do{
-		cout<<"1. Agregar contacto\nOpcion: ";
+		cout<<"1. Agregar contacto\n2. Eliminar un contacto\nOpcion: ";
 		cin>>op;
 		switch(op){
 			case 1:agregarContacto(contactos,n);break;
+			case 2:eliminarContacto(contactos,n);break;
 		}
 	}while(true);
 	return 0;
